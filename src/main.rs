7898138066bd9a085235c20b8b4d545b19aa06c3
SuriyaRuk <65972983+SuriyaRuk@users.noreply.github.com>
2025-04-11 11:19:21 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use tokio::net::TcpListener;
-use hyper::{Body, StatusCode};
+use hyper::{StatusCode};
 
 // This would normally come from the `hyper-util` crate, but we can't depend
 // on that here because it would be a cyclical dependency.
